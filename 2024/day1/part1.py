4185a80ab51ce2a300d5681fd52f31eae9187c24
James <jamespazcundy@gmail.com>
2024-12-05 17:33:12 +0000

    tidy up day 4

--- 2024/day1/part1.py
+++ 2024/day1/part1.py
@@ -3,7 +3,7 @@ import re
 def main():
     with open("in.txt", "r", encoding="utf-8") as f:
         content = f.read()
-    
+
     nums = re.split("\s+", content)
 
     left = []
@@ -17,12 +17,13 @@ def main():
             right.append(int(num))
     left.sort()
     right.sort()
-    
+
     delta_total = 0
     for l, r in zip(left, right):
         delta_total += abs(l-r)
-    
+
     print(delta_total)
 
 if __name__ == "__main__":
     main()
+    # 2530
