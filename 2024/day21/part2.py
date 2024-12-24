12da55a70f886cc7384d0883fe223545204307f3
JamesPCVR <jamespazcundy@gmail.com>
2024-12-24 00:18:21 +0000

    day 21 2024

--- 2024/day21/part2.py
+++ 2024/day21/part2.py
@@ -12,25 +12,20 @@ keypad_numeric = {
                  "0": (1, 3), "A": (2, 3)
 }
 
-class hashabledict(dict):
-    def __hash__(self):
-        return hash(tuple(sorted(self.items())))
-
 def coord_diff(a:tuple[int,int], b:tuple[int,int]) -> tuple[int,int]:
     """a - b"""
     x = a[0] - b[0]
     y = a[1] - b[1]
     return x, y
 
-@lru_cache(maxsize=None)
-def robot(moves:str, depth:int, keypad:hashabledict[str,tuple[int,int]]) -> str:
-    """recursive find optimal path"""
+def robot(moves:str, depth:int, keypad:dict[str,tuple[int,int]]) -> int:
+    """recursive find optimal path length"""
     # exit condition
     if depth == 0:
-        return moves
+        return len(moves)
 
     x, y = keypad["A"]
-    pattern = ""
+    pattern_length = 0
 
     for step in moves:
         x_n, y_n = keypad[step]
@@ -45,37 +40,42 @@ def robot(moves:str, depth:int, keypad:hashabledict[str,tuple[int,int]]) -> str:
 
         if (x + dx, y) not in keypad.values():
             # applying x first puts robot over empty
-            pattern += robot(y_first, depth - 1, hashabledict(keypad_directional))
+            pattern_length += robot_directional(y_first, depth - 1)
         elif (x, y + dy) not in keypad.values():
             # applying y first puts robot over empty
-            pattern += robot(x_first, depth - 1, hashabledict(keypad_directional))
+            pattern_length += robot_directional(x_first, depth - 1)
         else:
             # either could be applied first, find the optimal one
-            option_1 = robot(x_first, depth - 1, hashabledict(keypad_directional))
-            option_2 = robot(y_first, depth - 1, hashabledict(keypad_directional))
-            pattern += min(option_1, option_2)
+            option_1 = robot_directional(x_first, depth - 1)
+            option_2 = robot_directional(y_first, depth - 1)
+            pattern_length += min(option_1, option_2)
 
         # update position
         x, y = x_n, y_n
-    return pattern
+    return pattern_length
+
+@lru_cache(maxsize=None)
+def robot_directional(moves:str, depth:int) -> int:
+    """recursive optimal path length for directional keypad"""
+    return robot(moves, depth, keypad_directional)
+
+def robot_numeric(moves:str, depth:int) -> int:
+    """recursive optimal path length for numeric keypad"""
+    return robot(moves, depth, keypad_numeric)
 
 def main():
     with open("in.txt", "r", encoding="utf-8") as f:
         codes = f.read().split("\n")
 
-    STAGES = 2
+    STAGES = 25
 
     complexities = 0
-    for depth in range(STAGES + 2):
-        complexities = 0
-        for code in codes:
-            moves = robot(code, depth, hashabledict(keypad_numeric))
-            length = len(moves)
-            number = int(code.removesuffix("A"))
-            complexities += length * number
+    for code in codes:
+        length = robot_numeric(code, STAGES + 1)
+        number = int(code.removesuffix("A"))
+        complexities += length * number
 
     print(complexities)
 
 if __name__ == "__main__":
     main()
-    # 219254
